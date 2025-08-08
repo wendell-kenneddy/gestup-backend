bba13a237f85a30008f14f4f4068d63878079d08
Wendell Kenneddy <wkenneddy505@gmail.com>
2025-08-08 18:20:52 -0300

    add UserDetailsServiceImpl UserDetailsService implementation

---
+++ src/main/java/br/com/gestup/gestup/security/UserDetailsServiceImpl.java
@@ -0,0 +1,25 @@
+package br.com.gestup.gestup.security;
+
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import br.com.gestup.gestup.repository.UserRepository;
+
+@Service
+public class UserDetailsServiceImpl implements UserDetailsService {
+    private final UserRepository userRepository;
+
+    public UserDetailsServiceImpl(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        return userRepository
+                .findByEmail(username)
+                .map(UserDetailsImpl::new)
+                .orElseThrow(() -> new UsernameNotFoundException("Invalid credentials."));
+    }
+}
