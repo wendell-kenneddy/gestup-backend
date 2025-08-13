0d09ab216e3dcab515ee6460a4f837a9fd471fe7
Wendell Kenneddy <wkenneddy505@gmail.com>
2025-08-13 19:41:52 -0300

    remove blank lines in CreateUserRequest and LoginRequest dtos

--- src/main/java/br/com/gestup/gestup/dto/request/CreateUserRequest.java
+++ src/main/java/br/com/gestup/gestup/dto/request/CreateUserRequest.java
@@ -1,5 +1,4 @@
 package br.com.gestup.gestup.dto.request;
 
 public record CreateUserRequest(String username, String email, String password) {
-
 }
--- src/main/java/br/com/gestup/gestup/dto/request/LoginRequest.java
+++ src/main/java/br/com/gestup/gestup/dto/request/LoginRequest.java
@@ -1,5 +1,4 @@
 package br.com.gestup.gestup.dto.request;
 
 public record LoginRequest(String email, String password) {
-
 }
